e0869bd5d8970fbfbfd74a7b3339266a82666f29
KMarkov <kosty.markov.1997@gmail.com>
2024-11-19 10:32:09 +0400

    init

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: 'https://markteem.github.io/'
+  /* config options here */
+};
+
+export default nextConfig;
